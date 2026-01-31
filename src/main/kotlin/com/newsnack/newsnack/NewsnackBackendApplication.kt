96c28da26f31b98286d80c440b8a7c813f946142
Taehoon Kim <taehoonkim.dev@gmail.com>
2026-02-01 01:17:43 +0900

    chore: 시간대 관련 설정

--- src/main/kotlin/com/newsnack/newsnack/NewsnackBackendApplication.kt
+++ src/main/kotlin/com/newsnack/newsnack/NewsnackBackendApplication.kt
@@ -1,12 +1,19 @@
 package com.newsnack.newsnack
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import java.util.TimeZone
 
 @EnableJpaAuditing
 @SpringBootApplication
-class NewsnackBackendApplication
+class NewsnackBackendApplication {
+    @PostConstruct
+    fun init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+    }
+}
 
 fun main(args: Array<String>) {
 	runApplication<NewsnackBackendApplication>(*args)
--- src/main/kotlin/com/newsnack/newsnack/global/common/BaseEntity.kt
+++ src/main/kotlin/com/newsnack/newsnack/global/common/BaseEntity.kt
@@ -4,18 +4,18 @@ import jakarta.persistence.*
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
-import java.time.LocalDateTime
+import java.time.OffsetDateTime
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
 abstract class BaseEntity {
     @CreatedDate
     @Column(nullable = false, updatable = false)
-    var createdAt: LocalDateTime = LocalDateTime.now()
+    var createdAt: OffsetDateTime = OffsetDateTime.now()
         protected set
 
     @LastModifiedDate
     @Column(nullable = false)
-    var updatedAt: LocalDateTime = LocalDateTime.now()
+    var updatedAt: OffsetDateTime = OffsetDateTime.now()
         protected set
 }
