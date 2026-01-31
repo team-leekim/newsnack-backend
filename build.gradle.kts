1fc3d02c81b8bc83ea78847a977ad66caec0e0a9
Taehoon Kim <taehoonkim.dev@gmail.com>
2026-01-31 23:50:05 +0900

    chore: 애플리케이션 설정 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,12 +21,14 @@ repositories {
 }
 
 dependencies {
+    implementation ("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("tools.jackson.module:jackson-module-kotlin")
     implementation("io.hypersistence:hypersistence-utils-hibernate-71:3.15.1")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 
