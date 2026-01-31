d97b00ace1a60e3d879f1ed869a0636e884b11ff
Taehoon Kim <taehoonkim.dev@gmail.com>
2026-01-31 23:10:50 +0900

    chore: JPA Auditing 활성화 및 BaseEntity 추가

--- src/main/kotlin/com/newsnack/newsnack/NewsnackBackendApplication.kt
+++ src/main/kotlin/com/newsnack/newsnack/NewsnackBackendApplication.kt
@@ -2,7 +2,9 @@ package com.newsnack.newsnack
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableJpaAuditing
 @SpringBootApplication
 class NewsnackBackendApplication
 
