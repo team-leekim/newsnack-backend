ed6b5e0c893d94781d17e8132a651f8a03b97a5a
Taehoon Kim <taehoonkim.dev@gmail.com>
2026-02-01 15:32:38 +0900

    refactor: 조회 성능 최적화를 위해 ReactionCount에 totalCount 컬럼 추가

--- src/main/kotlin/com/newsnack/newsnack/domain/reaction/ReactionCount.kt
+++ src/main/kotlin/com/newsnack/newsnack/domain/reaction/ReactionCount.kt
@@ -15,10 +15,21 @@ class ReactionCount(
     @JoinColumn(name = "article_id")
     val article: AiArticle,
 
+    @Column(name = "total_count") var totalCount: Int = 0,
     @Column(name = "happy_count") var happyCount: Int = 0,
     @Column(name = "surprised_count") var surprisedCount: Int = 0,
     @Column(name = "sad_count") var sadCount: Int = 0,
     @Column(name = "angry_count") var angryCount: Int = 0,
     @Column(name = "empathy_count") var empathyCount: Int = 0
-) : BaseEntity()
-
+) : BaseEntity() {
+    fun addReaction(type: ReactionType) {
+        when (type) {
+            ReactionType.HAPPY -> happyCount++
+            ReactionType.SURPRISED -> surprisedCount++
+            ReactionType.SAD -> sadCount++
+            ReactionType.ANGRY -> angryCount++
+            ReactionType.EMPATHY -> empathyCount++
+        }
+        totalCount++
+    }
+}
